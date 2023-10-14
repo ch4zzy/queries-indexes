70920a264681a68dadbb989ed8b7914a72f1138f
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-10-14 15:51:05 +0300

    queries app

---
+++ apps/queries/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.postgres.fields import ArrayField
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    slug = models.SlugField(max_length=255, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name="products", on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Order(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    address = models.CharField(max_length=255)
+    postal_code = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    paid = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    order_json = models.JSONField()
+    status = ArrayField(models.CharField(max_length=255), blank=True, default=dict)
